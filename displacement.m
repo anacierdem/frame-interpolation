b8dc0f8d2941c2cc62c284378d69727f58295b0f
Ali Naci Erdem <anacierdem@gmail.com>
2017-05-28 14:06:22 +0300

    TODO: implement bands

--- displacement.m
+++ displacement.m
@@ -1,6 +1,7 @@
 L = 5; %pyramid levels
 LAMBDA = 1.2;
 TAU = 0.2;
+STEP = 0.1;
 
 v = VideoReader('xylophone.mp4');
 v.CurrentTime = 0;
@@ -13,39 +14,35 @@ clear currentPhase prevPhase
 
 while hasFrame(v) && frameCount < 2
     
-    frame = im2double(readFrame(v));
+    frame = readFrame(v);
 
-    %RGB channels
-    for i=1:3
-        [pyr,pind] = buildSCFpyr(frame(:,:,i),L,1);
-
-        if ~exist('currentPhase', 'var')
-            currentPhase = zeros([size(pyr,1), 3]);
-        end
-        
-%         showSpyr(pyr, pind);
-        
-        currentPhase(:,i) = angle(pyr);
-
-    end
+    %TODO: use Lab rgb2lab();
+    [pyr,pind] = buildSCFpyr(im2double(rgb2gray(frame)),L,1);
+    currentAmplitude = abs(pyr);
+    currentPhase = angle(pyr);
     
     if exist('prevPhase', 'var')
         phaseDiff = prevPhase - currentPhase;
         diff = atan2(sin(phaseDiff),cos(phaseDiff));
         
+        phiHat = zeros(size(currentPhase));
         clear prevPhaseBand1
         
         numLevels = spyrHt(pind);
-        for currentLevel = numLevels:-1:2 %1 is high freq. residuals
+        for currentLevel = numLevels:-1:1
             %TODO: for all bands
-            spyrNumBands(pind);
+            numBands = spyrNumBands(pind);
+            
+            for currentBand = 1:numBands
+                
+            end
             
             phaseBand1 = spyrBand(currentPhase, pind, currentLevel, 1);
 
-            if exist('prevPhaseBand1', 'var')
+            if exist('prevLevelPhaseBand1', 'var')
                 %Band 1
-                resizedPrevBand1 = imresize(prevPhaseBand1, size(phaseBand1));
-                phaseDiff = resizedPrevBand1 - LAMBDA * phaseBand1;
+                resizedPrevLevelBand1 = imresize(prevLevelPhaseBand1, size(phaseBand1));
+                phaseDiff = resizedPrevLevelBand1 - LAMBDA * phaseBand1;
                 phi = atan2(sin(phaseDiff),cos(phaseDiff));
                 
                 %TODO
@@ -53,36 +50,58 @@ while hasFrame(v) && frameCount < 2
 %                 resizedPrevBand1 > phiLimit
                 
                 indices = abs(phi) > pi/2;
-                resizedPrevBand1(indices) = phaseBand1(indices);
+                resizedPrevLevelBand1(indices) = phaseBand1(indices);
                 
                 %Adjust phase
                 currentLevelDiff = spyrBand(diff, pind, currentLevel, 1);
                 maxValue = ceil(size(phaseBand1, 1) / 2);
                 allValues = zeros([size(currentLevelDiff),maxValue+1]);
                 for gamma = 0:1:maxValue; 
-                    allValues(:,:,gamma+1) = (resizedPrevBand1 - (currentLevelDiff + gamma * 2 *pi)) .* (resizedPrevBand1 - (currentLevelDiff + gamma * 2 *pi));
+                    allValues(:,:,gamma+1) = (resizedPrevLevelBand1 - (currentLevelDiff + gamma * 2 *pi)) .* (resizedPrevLevelBand1 - (currentLevelDiff + gamma * 2 *pi));
                 end
-                
+                %second 1 is current band
+                currentIndices = pyrBandIndices(pind,1 + 1 + numBands*(currentLevel-1));
                 [~, gammaStar] = min(allValues, [], 3);
-                phiHat = currentLevelDiff + (2*(gammaStar-1)*pi);
-                
-                %Interpolate phase
-%                 phiAlpha = 
-
+                phiHat(currentIndices) = currentLevelDiff + (2*(gammaStar-1)*pi);
             end
             
-            prevPhaseBand1 = phaseBand1;
+
+            prevLevelPhaseBand1 = phaseBand1;
 
 %             subplot(2, numLevels, (currentLevel-1)*2 + 1);
 %             imshow(currentVal, []);
 %             subplot(2, numLevels, (currentLevel-1)*2 + 2);
 %             imshow(currentPhase, [-pi, pi]);
         end
+        
+        %Interpolate phase and reconstruct
+        imageIndex = 1;
+        for alpha = 0:STEP:1
+            phiAlpha = prevPhase + alpha.*phiHat;
+            resultingAmplitude = prevAmplitude * (1-alpha) + currentAmplitude * alpha;
+            
+            %Chose closer high frequency residual
+            hfIndices = pyrBandIndices(pind,band);
+            if(alpha < 0.5)
+                resultingAmplitude(hfIndices) = prevAmplitude(hfIndices);
+            else
+                resultingAmplitude(hfIndices) = currentAmplitude(hfIndices);
+            end
+            
+            resultingPyr = resultingAmplitude + 1i*phiAlpha;
+            res = reconSFpyr(resultingPyr,pind);
+            
+            subplot(2, size(0:STEP:1), imageIndex);
+            imshow(res);
+            imageIndex = imageIndex + 1;
+        end
+            
         drawnow
     end
     
     prevPhase = currentPhase;
+    prevAmplitude = currentAmplitude;
     frameCount = frameCount + 1;
 end
 
-res = reconSFpyr(pyr,pind);
+
